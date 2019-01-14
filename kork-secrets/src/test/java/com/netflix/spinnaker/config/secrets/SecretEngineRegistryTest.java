7b190429bb8183793de451c390b135468a368868
Cameron Motevasselani <cameron.motevasselani@armory.io>
2019-01-14 02:49:25 -0800

    feat(kork/secrets): adding S3BucketEngine

---
+++ kork-secrets/src/test/java/com/netflix/spinnaker/config/secrets/SecretEngineRegistryTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 Armory, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.config.secrets;
+
+
+import com.netflix.spinnaker.config.secrets.engines.S3BucketEngine;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {SecretsConfiguration.class})
+public class SecretEngineRegistryTest {
+
+  @Autowired
+  SecretEngineRegistry secretEngineRegistry;
+
+  @Test
+  public void secretEnginesRegister() {
+    List<SecretEngine> secretEngines = secretEngineRegistry.getSecretEngineList();
+    assertEquals(true, secretEngines.size() >= 1);
+  }
+
+}
+
